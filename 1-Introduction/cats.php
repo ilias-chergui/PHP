33c17fa6216c3990d3676a623579b52418910113
ilias-chergui <chergui.ilias@gmail.com>
2017-11-06 09:12:08 +0100

    upload exo 1

---
+++ 1-Introduction/cats.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exo Php</title>
+</head>
+<body>
+  <?php echo "Voici un petit chat !";
+
+  $image = "http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg";
+
+  print '<img src="'.$image.'" alt="un chat" />';
+
+  echo '<p><a href="http://localhost/ExoPhp/1-Introduction/index.php"> Link to Ball </a></p>';
+  ?>
+</body>
+</html>
---
+++ 1-Introduction/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exo Php</title>
+</head>
+<body>
+  <?php echo "Coucou moi !";
+
+  $image = "https://images-na.ssl-images-amazon.com/images/I/61Y68Yo5v%2BL._SY355_.jpg";
+
+  print '<img src="'.$image.'" alt="un ballon" />';
+
+  echo '<p><a href="http://localhost/ExoPhp/1-Introduction/cats.php"> Link to Cats </a></p>';
+  ?>
+</body>
+</html>
